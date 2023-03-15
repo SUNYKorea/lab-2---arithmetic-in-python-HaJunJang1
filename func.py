335294b5d2308340a6f5c7579c7ae028c98ff2ea
Hajun Jang <hajun.jang@stonybrook.edu>
2023-03-15 14:09:00 +0900

    negative function solved

--- func.py
+++ func.py
@@ -7,22 +7,36 @@ def add(x, y):
 #       return the square root of the given number. 
 
 def neg(x):
-    ... # fill here
+    return - x
 
 def sqrt(x):
-    ... # fill here
+    return x^(1/2)
 
+def sub(x, y):
+    return x - y
+
+def div(x, y):
+    return x / y
+
+def mult(x, y):
+    return x * y
+
+def exp(x, y):
+    return x ** y
 # -------------------------------------- Task 2 -----------------------------------
 
 # TODO: Implement the quadratic formula using *only* the functions defined here.
 #       Do not use arithmetic operators directly. 
 #       You're allowed to define other functions.
-a = 1
-b = -3
-c = 1
 
-x1 = ... # TODO: write a code to compute the first root of the quadratic equation
-x2 = ... # TODO: then do the same for the second root
+def function (x, y):
+    a*(x**2) + b*x*y + c
+a = 1
+b = 4
+c = 4
+
+x1 = div(add(neg(b), sqrt(sub(exp(b, 2), 4*a*c))), 2*a) # TODO: write a code to compute the first root of the quadratic equation
+x2 = div(sub(neg(b), sqrt(sub(exp(b, 2), 4*a*c))), 2*a) # TODO: then do the same for the second root
 # Note: Make sure to remove the ellipsis (...) when you're writing your code
 
 print("First root:" + str(x1))
